ee4b3d165f77e9d78dc28f68b1e91df1e605dc7d
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-01-22 22:50:56 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/app.dart';
+import 'package:restaurant_app/data/api/api_services.dart';
+import 'package:restaurant_app/provider/detail/new_restaurant_review_provider.dart';
+import 'package:restaurant_app/provider/detail/restaurant_detail_provider.dart';
+import 'package:restaurant_app/provider/home/restaurant_list_provider.dart';
+import 'package:restaurant_app/provider/search/restaurant_search_provider.dart';
 import 'package:restaurant_app/provider/theme/theme_mode_provider.dart';
 
 void main() {
@@ -9,6 +14,29 @@ void main() {
       providers: [
         ChangeNotifierProvider(
           create: (_) => ThemeModeProvider(),
+        ),
+        Provider<ApiServices>(
+          create: (_) => ApiServices(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => RestaurantListProvider(
+            context.read<ApiServices>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => RestaurantDetailProvider(
+            context.read<ApiServices>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => RestaurantSearchProvider(
+            context.read<ApiServices>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => NewRestaurantReviewProvider(
+            context.read<ApiServices>(),
+          ),
         ),
       ],
       child: const App(),
