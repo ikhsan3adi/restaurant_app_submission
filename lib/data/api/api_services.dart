64d4e9002361a53ec3f100317a712bd8b3d11e45
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-01-22 22:36:10 +0700

    Update ApiServices

--- lib/data/api/api_services.dart
+++ lib/data/api/api_services.dart
@@ -1,9 +1,65 @@
-import 'package:restaurant_app/data/enums/image_size.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:restaurant_app/data/enum/image_size.dart';
+import 'package:restaurant_app/data/model/new_restaurant_review.dart';
+import 'package:restaurant_app/data/model/new_restaurant_review_response.dart';
+import 'package:restaurant_app/data/model/restaurant_detail_response.dart';
+import 'package:restaurant_app/data/model/restaurant_list_response.dart';
+import 'package:restaurant_app/data/model/restaurant_search_response.dart';
 
 class ApiServices {
   static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   static String restaurantImageUrl(String pictureId, ImageSize size) {
     return '$_baseUrl/images/${size.name}/$pictureId';
+  }
+
+  Future<RestaurantListResponse> getRestaurantList() async {
+    final response = await http.get(Uri.parse('$_baseUrl/list'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load restaurant list');
+    }
+
+    return RestaurantListResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<RestaurantDetailResponse> getRestaurantDetail(String id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/detail/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load restaurant detail');
+    }
+
+    return RestaurantDetailResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<RestaurantSearchResponse> getRestaurantSearchResult(
+    String query,
+  ) async {
+    final response = await http.get(Uri.parse('$_baseUrl/search?q=$query'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to search restaurant');
+    }
+
+    return RestaurantSearchResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<NewRestaurantReviewResponse> postRestaurantReview(
+    NewRestaurantReview review,
+  ) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/review'),
+      body: review.toJson(),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception('Failed to add review');
+    }
+
+    return NewRestaurantReviewResponse.fromJson(jsonDecode(response.body));
   }
 }
