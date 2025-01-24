8b7812e39858c936e2b117f26f0ea379c774c7c1
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-01-24 15:53:52 +0700

    Update restaurant_search_response.dart

--- lib/data/model/restaurant_search_response.dart
+++ lib/data/model/restaurant_search_response.dart
@@ -2,14 +2,11 @@ import 'package:restaurant_app/data/model/restaurant.dart';
 
 class RestaurantSearchResponse {
   final bool error;
-  //? TODO: missing message?
-  // final String? message;
   final int founded;
   final List<Restaurant> restaurants;
 
   const RestaurantSearchResponse({
     required this.error,
-    // this.message,
     required this.founded,
     required this.restaurants,
   });
@@ -17,7 +14,6 @@ class RestaurantSearchResponse {
   factory RestaurantSearchResponse.fromJson(Map<String, dynamic> json) {
     return RestaurantSearchResponse(
       error: json['error'],
-      // message: json['message'],
       founded: json['founded'],
       restaurants: List<Restaurant>.from(
         json['restaurants'].map((x) => Restaurant.fromJson(x)),
@@ -27,7 +23,6 @@ class RestaurantSearchResponse {
 
   Map<String, dynamic> toJson() => {
         'error': error,
-        // 'message': message,
         'founded': founded,
         'restaurants': List<dynamic>.from(
           restaurants.map((x) => x.toJson()),
