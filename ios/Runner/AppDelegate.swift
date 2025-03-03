ff171abd0d35d0e8ac39cb1e3b8e9097d0330209
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-03-03 19:19:58 +0700

    chore(deps): Add notification & work_manager package

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,24 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+      UIApplication.shared.cancelAllLocalNotifications()
+      UserDefaults.standard.set(true, forKey: "Notification")
+    }
+  
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+  
+    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
+  
+    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "com.example.restaurant_app", frequency: NSNumber(value: 16 * 60))
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
